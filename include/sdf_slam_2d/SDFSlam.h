f496a35a931e8c966c3547d7aedb028573b5a370
Joscha F <jodafo@gmail.com>
2015-05-19 17:19:07 +0100

    Cleaned up SDFSlam.cpp/h

--- include/sdf_slam_2d/SDFSlam.h
+++ include/sdf_slam_2d/SDFSlam.h
@@ -24,35 +24,22 @@
 #include "map/VectorMap.h"
 #include "map/OccupancyGrid.h"
 
-
 #include "registration/AbstractRegistration.h"
 #include "registration/GaussNewton.h"
 
 #include "utility/Types.h"
 #include "utility/UtilityFunctions.h"
 
-
-#include <iostream>
-#include <fstream>
-#include <cmath>
-
-#include "ros/ros.h"
-
-#include "geometry_msgs/PoseStamped.h"
-#include "sensor_msgs/LaserScan.h"
-#include "sensor_msgs/PointCloud2.h"
-#include "nav_msgs/OccupancyGrid.h"
-
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/PointCloud2.h>
 #include <std_srvs/Empty.h>
-
-#include "tf_conversions/tf_eigen.h"
-#include "tf/transform_listener.h"
-#include "tf/transform_broadcaster.h"
-
-#include "laser_geometry/laser_geometry.h"
-
+#include <tf_conversions/tf_eigen.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
+#include <laser_geometry/laser_geometry.h>
 #include <pcl_conversions/pcl_conversions.h>
-#include <pcl/point_types.h>
 #include <pcl_ros/transforms.h>
 
 namespace sdfslam {
@@ -65,83 +52,10 @@ namespace sdfslam {
 
         ~SignedDistanceField();
 
-        void publishMap(bool updated);
-
-        void publishMarkerMap();
-
-        void scanCb(const sensor_msgs::LaserScan::ConstPtr& scan);
-
-        void raytrace(float *src, float tars[][2], int num, VecMapFloat &map);
-
-        double round(double Zahl, unsigned int Stellen);
-//
-//        inline Eigen::Vector2f toMap(const Eigen::Vector2f &x);
-//
-//        inline Eigen::Vector2f toRl(const Eigen::Vector2f &x);
-//
-//        inline void toMap(float *x);
-//
-//        inline float toMap(const float &x, const int &s);
-//
-//        inline void toRl(float *x);
-//
-//        inline PointType genPoint(float xPos, float yPos);
-//
-//        inline float p2lDist(float m, float b, const Eigen::Vector2f &p);
-//
-//        inline float p2lDist(float m, float b, const float *p);
-//
-//        inline float p2pDist(float *p1, float *p2);
-//
-//        inline float p2pDist(float p1x, float p1y, float p2x, float p2y);
-//
-//        inline float p2pDist(float p1x, float p1y, float *p2);
-//
-//
-//
-//        float p2lsDist(float *v, float *w, float *p);
-//
-//        float p2lsDist2(float *v, float *w, float *p);
-
-      //  inline float xVal(float y, float m, float b);
-
-        //inline float yVal(float x, float m, float b);
-
         bool checkTimeout();
 
-        bool invertCheck(float *src, float *tar, float *pos, float m, float b);
-
-        bool invertCheck(float *src, float *tar, float pos[2][2]);
-
-
-        bool estimateTransformationLogLh(Eigen::Vector3f &estimate,
-                                         PCLPointCloud const &cloud,
-                                         float *searchfacs,
-                                         VecMapFloat const &aMap,
-                                         int *case_count,
-                                         bool fine);
-
-        Eigen::Vector3f newMatch(const PCLPointCloud &cloudIn,
-                                 const VecMapFloat &aMap,
-                                 int *case_count);
-
-        void getCompleteHessianDerivs(const Eigen::Vector3f &pose,
-                                      PCLPointCloud const &cloud,
-                                      Eigen::Matrix3f &H,
-                                      Eigen::Vector3f &dTr,
-                                      VecMapFloat const &aMap,
-                                      int *case_count,
-                                      bool fine);
-
-        int mapValues(const Eigen::Vector2f &coords,
-                      VecMapFloat const &aMap,
-                      float *mpdxdy,
-                      bool fine);
-
-
-        void saveMap(std::string filename);
-
-        void loadMap(std::string filename);
+    protected:
+        void scanCb(const sensor_msgs::LaserScan::ConstPtr& scan);
 
         bool saveMapService(std_srvs::Empty::Request &req, std_srvs::Empty::Request &res);
 
@@ -153,124 +67,73 @@ namespace sdfslam {
 
         bool updateMapService(std_srvs::Empty::Request &req, std_srvs::Empty::Request &res);
 
+        bool createAndPublishVisualMap(std_srvs::Empty::Request &req, std_srvs::Empty::Request &res);
+
         void checkTodos();
 
-
-    protected:
+        ros::Duration update_map(AbstractMap* aMap, const PCLPointCloud& pc, const Eigen::Vector3d& pose3d);
+        ros::Duration publish_map(AbstractMap* aMap);
 
         AbstractMap* map_;
         AbstractMap* visualization_map_;
+
         AbstractRegistration* registration_;
 
+        ros::NodeHandle nh_;
 
         ros::ServiceServer serviceSaveMap;
         ros::ServiceServer serviceLoadMap;
         ros::ServiceServer serviceUpdateMap;
+        ros::ServiceServer serviceCreateAndPublishVisualMap;
         ros::ServiceServer servicePublishMap;
         ros::ServiceServer serviceReset;
 
-        int debugcounter;
-
-        void updateMap(int *pos, float val, VecMapFloat &aMap, bool override);
-
-        void map(PCLPointCloud &pc, VecMapFloat &aMap);
-
-        void regression_map(PCLPointCloud &pc, VecMapFloat &aMap);
-
-        void update_cells(float *src, float tars[][2], int num, float *window, VecMapFloat &map);
-
-        void deming_regression(float tars[][2], int num, double &beta0, double &beta1);
-
-        void simple_raytrace(Eigen::Vector2d src, Eigen::Vector2d tar, VecMapFloat &aMap);
-
-        ros::NodeHandle nh_;
         ros::Subscriber scan_sub_;
-        ros::Publisher map_pub_;
-        ros::Publisher occ_map_pub_;
         ros::Publisher tfpub_;
-        ros::Publisher iso_pub_;
-        ros::Publisher cloudA_pub_;
-        ros::Publisher cloudB_pub_;
-
+        ros::Publisher scan_cloud_pub_;
 
         tf::TransformListener tf_;
         tf::TransformBroadcaster tfbr_;
 
         laser_geometry::LaserProjection projector_;
 
-        pcl::PointCloud<pcl::PointXYZ> cloud_map_;
-        pcl::PointCloud<pcl::PointXYZ> iso_map_; //rm me
-        VecMapFloat sdf_;
-        VecMapFloat occ_map_;
-        std::vector<std::vector<int> > sdf_count_;
-        std::vector<std::vector<int> > sdf_level_;
-        std::vector<std::vector<int> > sdf_thresholded_;
-        //  sensor_msgs::PointCloud laser_point_cloud_;
-
         std::string p_scan_topic_;
-        std::string p_base_frame_;
-
         std::string p_robot_frame_;
         std::string p_tar_frame_;
         std::string p_fixed_frame_;
-
-        Eigen::Matrix3f H;
-        Eigen::Vector3f dTr;
-
-        int p_avg_range_;
+        Eigen::Vector3d pos_;
+        Eigen::Vector3d lastMapPos_;
         int p_vanity_;
-        double p_reg_threshold_;
-        double p_min_range_;
-        double p_max_range_;
-        double p_stop_iter_time_;
-        bool p_perma_map_;
-        int p_scan_rays_;
-        int p_num_iter_;
-        bool p_fine_pos_;
-        bool p_avg_mapping_;
-        bool p_initial_pose_;
         int p_timeout_ticks_;
         int time_out_counter_;
         int not_converged_counter_;
-        float epsilon_;
-        int p_update_map_aoe_;
-        bool p_publish_map_;
-        bool map_empty_;
-        bool p_debug_;
-        int p_overshoot_;
-        float p_truncation_;
-        float p_weight_;
-        uint32_t p_scan_subscriber_queue_size_;
-        double p_grid_res_;
         int p_map_size_x_;
         int p_map_size_y_;
-        float p_isovalue_;
+        int p_scan_subscriber_queue_size_;
+        double p_time_warning_threshold;
+        double p_reg_threshold_;
         double p_yaw_threshold_;
         double p_dist_threshold_;
-        Eigen::Vector3f pos_;
-        Eigen::Vector3f lastMapPos_;
-        PCLPointCloud sameScan;
-        float p_interpl_radius_;
+        double p_grid_res_;
+        bool p_perma_map_;
+        bool p_initial_pose_;
+        bool p_publish_map_;
+        bool map_empty_;
         bool pose_estimation_;
-        bool mapping_;
+        bool p_mapping_;
         bool map_flag_;
-
-    private:
-        bool searchdirplus[3];
-
-        //
-        bool printDur_;
-        bool overrideMap_;
         bool updateMapServiceCalled_;
         bool publishMapServiceCalled_;
         bool loadMapServiceCalled_;
         bool converged_;
-        bool searchBoost_;
-        double tempYaw_;
-        double deltaXSum_;
-        double deltaYSum_;
+
+    private:
+
     };
 
 }
 
 #endif
+
+
+
